7f5112382d48edcc0a6f266f3c22716e6d6fd587
StevenRodriguezDev <stevenrodriguez3913@gmail.com>
2023-09-21 23:07:28 -0600

    updated js

---
+++ index.js
@@ -0,0 +1,48 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateSVG = require("./lib/generateSVG");
+// TODO: Create an array of questions for user input
+const questions = [
+  //3 letters
+  {
+    type: "input",
+    message: "Please choose 3 letters: ",
+    name: "letters",
+  },
+  // colors
+  {
+    type: "input",
+    message: "Please enter a color keyword or a hexadecimal number: ",
+    name: "colors",
+  },
+  //shape
+  {
+    type: "list",
+    message: "Please choose a Shape: ",
+    name: "shape",
+    choices: ["Square", "Circle", "Triangle"],
+  },
+];
+
+// TODO: Create a function to write  file
+function createFile(fileName, data) {
+  const mySVG = generateSVG(data);
+  fs.writeFile(fileName, mySVG, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Your SVG file is in the output folder!");
+    }
+  });
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    createFile("./output/logo.svg", data);
+  });
+}
+
+// Function call to initialize app
+init();
